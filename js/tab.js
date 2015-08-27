34dcb12ea0eaf1e6e68601f7817a8b5ded4fef22
zhangxiaotianmacpor <zhangxiaotian@zhangxiaotian.local>
2015-08-27 16:03:51 +0800

    first

---
+++ js/tab.js
@@ -0,0 +1,23 @@
+var tab = function(mycfg) {
+	var cfg = {
+		tabId: '#tab_nav',
+		tabChild: 'li',
+		tabContentId: '#tab_content',
+		tabContentChid: '.tab_content_child',
+		tabClass: 'current'
+	}
+	$.extend(false, cfg, mycfg);
+	var tabBtn = $(cfg.tabId),
+		tabBtnChild = tabBtn.children(cfg.tabChild),
+		tabContent = $(cfg.tabContentId),
+		tabContetChild = tabContent.children(cfg.tabContentChid);
+	var tabFn = function() {
+		tabBtnChild.bind('click', function() {
+			var _i = $(this).index();
+			$(this).addClass(cfg.tabClass).siblings(cfg.tabChild).removeClass(cfg.tabClass)
+			tabContetChild.eq(_i).show().siblings(cfg.tabContentChid).hide()
+		})
+	}
+	tabFn()
+
+}
